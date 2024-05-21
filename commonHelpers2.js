067ec0528209eb7f0a014519924308fe5c5cbad0
Mikhail-Mukha <152735000+Mikhail-Mukha@users.noreply.github.com>
2024-05-21 20:56:50 +0000

    Deploying to gh-pages from @ Mikhail-Mukha/goit-js-hw-10@475e1a19e1b74a3c1c6f57c17dae6230f9840ee4 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/reset-df635d97.js";import{i as o}from"./assets/vendor-77e16229.js";const i=document.querySelector(".form");i.addEventListener("submit",e=>{e.preventDefault();const s=i.elements.delay.value,r=i.elements.state.value;m(s,r).then(t=>{o.success({title:"Success",message:`✅ Fulfilled promise in ${t}ms`})}).catch(t=>{o.error({title:"Error",message:`❌ Rejected promise in ${t}ms`})})});function m(e,s){return new Promise((r,t)=>{setTimeout(()=>{s==="fulfilled"?r(e):t(e)},e)})}
+//# sourceMappingURL=commonHelpers2.js.map
